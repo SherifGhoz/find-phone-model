9931ad0c33145d10a0a15d2905e3d7f7c73b2034
GhozDev <48717360+SherifGhoz@users.noreply.github.com>
2020-05-19 03:39:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ exports.findPhone = function (searchString) {
   const result = [];
   for (let item of phoneList) {
     for (let field of item) {
+      console.log(result)
       if (field.toString().toLowerCase().indexOf(searchString.toLowerCase()) !== -1) {
         result.push({ 'Retail Branding': item[0], 'Marketing Name': item[1], 'Device': item[2], 'Model': item[3] });
         break;
