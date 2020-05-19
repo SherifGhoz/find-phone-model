db052143086b92c7c2dca272ff49cd373fcb5ddb
GhozDev <48717360+SherifGhoz@users.noreply.github.com>
2020-05-19 04:01:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const phoneList = require('./phone-list.js');
 
 exports.findPhone = function (searchString) {
   const result = [];
-  console.log(result);
+  console.log('before', result);
   for (let item of phoneList) {
     for (let field of item) {
-      console.log(result)
       if (field.toString().toLowerCase().indexOf(searchString.toLowerCase()) !== -1) {
         result.push({ 'Retail Branding': item[0], 'Marketing Name': item[1], 'Device': item[2], 'Model': item[3] });
         break;
       }
     }
   }
+  console.log('after:', result)
   return result;
 }
