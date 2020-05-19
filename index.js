79d8513449911c69f70a6d7082d2229d8d9d3ec0
GhozDev <48717360+SherifGhoz@users.noreply.github.com>
2020-05-19 04:10:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const phoneList = require('./phone-list.js');
 
 exports.findPhone = function (searchString) {
   const result = [];
-  console.log('before', result);
   for (let item of phoneList) {
     for (let field of item) {
      if (field.toString().toLowerCase().replace(/\s+/g, "").indexOf(searchString.replace(/\s+/g, "").toLowerCase()) !== -1) {
@@ -12,6 +11,5 @@ exports.findPhone = function (searchString) {
       }
     }
   }
-  console.log('after:', result)
   return result;
 }
