bf2e00da1a95e47cbd5a50f023c396776d3f50ae
GhozDev <48717360+SherifGhoz@users.noreply.github.com>
2020-05-19 03:24:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use strict'
-var phoneList = require('./phone-list.js');
+const phoneList = require('./phone-list.js');
 
 exports.findPhone = function (searchString) {
-  var result = [];
-  for (var item of phoneList) {
-    for (var field of item) {
+  const result = [];
+  for (let item of phoneList) {
+    for (let field of item) {
       if (field.toString().toLowerCase().indexOf(searchString.toLowerCase()) !== -1) {
         result.push({ 'Retail Branding': item[0], 'Marketing Name': item[1], 'Device': item[2], 'Model': item[3] });
         break;
